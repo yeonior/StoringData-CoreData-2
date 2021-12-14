93a8bf6d7c9e570c0a0298421a2125b20716f89e
Rus <de.mr44@gmail.com>
2021-12-14 22:13:14 +0500

    Added fetchedResultsController and configured it

--- CoreDataExample2/TableViewController.swift
+++ CoreDataExample2/TableViewController.swift
@@ -10,9 +10,32 @@ import CoreData
 
 class TableViewController: UITableViewController {
     
+    let dataStoreManager = DataStoreManager()
+    var fetchedResultsController: NSFetchedResultsController<User>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureRFC()
+    }
+    
+    // fetchedResultsController configuring
+    private func configureRFC() {
+        let context = dataStoreManager.persistentContainer.viewContext
+        let fetchRequest = User.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        fetchRequest.fetchLimit = 15
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                                              managedObjectContext: context,
+                                                              sectionNameKeyPath: nil,
+                                                              cacheName: nil)
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("Failed to fetch entities: \(error)")
+        }
     }
     
     // MARK: - Table view data source
