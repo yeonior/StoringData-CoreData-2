ab1317160c68fc110768f546c2da011c38adc9d2
Rus <de.mr44@gmail.com>
2021-12-14 20:43:25 +0500

    Added DataStoreManager

--- CoreDataExample2/TableViewController.swift
+++ CoreDataExample2/TableViewController.swift
@@ -6,30 +6,30 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 0
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
         
-
+        
+        
         return cell
     }
 }
