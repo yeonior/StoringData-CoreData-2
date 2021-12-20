0751790fecc95d384eb5265b8c1d766248d10134
Rus <de.mr44@gmail.com>
2021-12-20 18:37:25 +0500

    Added user avatar attribute and the corresponding class to it

---
+++ CoreDataExample2/ImageTransformer.swift
@@ -0,0 +1,19 @@
+//
+//  ImageTransformer.swift
+//  CoreDataExample2
+//
+//  Created by ruslan on 20.12.2021.
+//
+
+import UIKit
+
+class ImageTransformer: ValueTransformer {
+    
+    override func transformedValue(_ value: Any?) -> Any? {
+        return nil
+    }
+    
+    override func reverseTransformedValue(_ value: Any?) -> Any? {
+        return nil
+    }
+}
